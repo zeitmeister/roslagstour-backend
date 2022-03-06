dc6cc4eb6869c9084f6203ae6b4943134e1d0bd9
Simon <simon@24HR203.onmicrosoft.com>
2022-03-06 19:44:18 +0100

    removing some profanity

--- src/main/java/com/example/demo/BusStopsApiApplication.java
+++ src/main/java/com/example/demo/BusStopsApiApplication.java
@@ -9,7 +9,6 @@ public class BusStopsApiApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(BusStopsApiApplication.class, args);
-		System.out.println("hejsan din fan");
 	}
 
 }
